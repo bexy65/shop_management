859b3f5e4d727696886406857b5b814a0f68780f
bexy65 <naimertan65@gmail.com>
2024-04-29 21:02:18 +0300

    add category method, logic

--- database_utils.py
+++ database_utils.py
@@ -1,9 +1,10 @@
 import sqlite3
 import hashlib
 
-class Database_client:
+class DatabaseClient:
     def __init__(self, db_file='myshop.db'):
         self.db_file = db_file
+      
 
     def _get_connection(self):
         return sqlite3.connect(self.db_file)
@@ -18,10 +19,17 @@ class Database_client:
                             )''')
             cursor.execute('''CREATE TABLE IF NOT EXISTS products (
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                name TEXT,
+                                name TEXT UNIQUE,
                                 category TEXT,
-                                price REAL
+                                price REAL,
+                                count INTEGER,
+                                FOREIGN KEY (category_id) REFERENCES categories(id)
                             )''')
+            cursor.execute('''CREATE TABLE IF NOT EXISTS categories (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT UNIQUE
+                            )''')
+            
 
     def create_user(self, username, password):
         if len(password) < 8 or ' ' in password:
@@ -41,5 +49,52 @@ class Database_client:
             user = cursor.fetchone()
             return bool(user)
 
-db = Database_client()
+    def add_category(self, name):
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO categories (name) VALUES (?)', (name,))
+            conn.commit()
+    
+    def get_categories(self):
+        try:
+            with self._get_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT name FROM categories')
+                categories = cursor.fetchall()
+                category_names = [category[0] for category in categories]
+                return category_names
+            
+        except Exception as e:
+            print("Error:", e)
+            return None
+    
+    
+    def get_products(self):
+        try:
+            with self._get_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT name, price, count, category FROM products')
+                products = cursor.fetchall()
+                print(products)
+                return products                
+        except Exception as e:
+            print('Error:', e)
+            return None
+    
+    def add_product(self, product):
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO products (name, category, price, count) VALUES (?, ?, ?, ?)', (product['name'], product['category'], product['price'], product['count']))
+            conn.commit()
+    
+    def remove_product(self, product_name):
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM products WHERE name=?', (product_name))
+            conn.commit()
+    
+ 
+            
+db = DatabaseClient()
 
+db.get_products()
